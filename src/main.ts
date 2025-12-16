ed0d2dd41a0da7a8313745b3dbb54236de32f778
Robin Ryan <robin1ryan@hotmail.com>
2025-12-16 19:47:10 +0200

    moving things around

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import "./style.css";
 
-// goal with assignment:
-// create a todo-list which has the following features:
-// add to list
-// remove from list
-// move up on list
-// move down on list
-// drag and drop
-// saved to localStorage
-// main will handle events
+const liItem = {
+  id: crypto.randomUUID(),
+  title: title,
+  completed: false,
+  createdAt: new Date(),
+};
+
+// event-page:
+// get input
+// add list item with new input
