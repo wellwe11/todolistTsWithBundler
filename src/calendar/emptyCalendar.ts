d5f746a96dcafcaf745aadd327014d152c3ef986
Robin Ryan <robin1ryan@hotmail.com>
2026-01-02 14:27:49 +0200

    building upcoming date

---
+++ src/calendar/emptyCalendar.ts
@@ -0,0 +1,19 @@
+// step 1
+// create a page selector
+// each page will be a month, which is based on a year
+// when a page is selected, a new date for that month is created
+// whenever the page changes, data is re-created, and a new calendar month is made
+
+// step 2
+// for each day in current month, a div container is created
+// each div container should have a day associated to it
+// each div container should have an id of day, month and year
+
+// step 3
+// each time a month is created, scan local storage for matching dates
+// each time a matching date is found, place it correctly on matching day
+
+// step 4
+// when user clicks on date, expand length of day div.
+// the div should contain a time-list
+// Events should be spread over the date to find matching time, and pin it to them
