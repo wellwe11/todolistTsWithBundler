7742fdf4a2ca465484ae1be34ad690e5bcee3edc
Robin Ryan <robin1ryan@hotmail.com>
2026-01-05 17:07:09 +0200

    minor upadte

--- src/calendar/mainCalendar.ts
+++ src/calendar/mainCalendar.ts
@@ -1,4 +1,4 @@
-import monthCalendar, { updateCalendarMonth } from "./calendar/monthCalendar";
+import { updateCalendarMonth } from "./calendar/monthCalendar";
 import weekCalendar from "./calendar/weekCalendar";
 import tabActions from "./tabController/tabActions";
 
@@ -86,36 +86,12 @@ export class CalendarData {
 export const currentDate: CalendarData = new CalendarData(new Date());
 export const activeCalendar: CalendarType = new CalendarType("week");
 
-// clicking today will display a list of todos for today
-// on top - a button to click back and forth, displaying for example tomorrow's todo-list, yesterdays, and so forth.
-
-// clicking week will display columns, containing todo's. Each day will have a time-frame, where todo's as sorted by time and associated to those specific time-rows
-// on top - a button to click which scrolls from previous week to next week
-// clicking on a specific day will take user to that day
-
-// clicking month will show a big grid showing all days, with minimized main-events
-// on top - a button to clikc which scrolls previous month, next month, etc
-// clicking on a specific day will take user to that day
-
-// priority:
-// create 3 buttons: today, week, month
-// create 2 buttons: next, previous
-
-// create a day component
-
-// create a week component
-
-// create a month component
-
-// used for when something is changed inside of the tasks-array. Recreates only specific elements
 export const updateCalendar = () => {
   const calendarDays = document.querySelector(
     "#calendarDays"
   ) as HTMLDivElement;
 
   const title = document.querySelector("#title") as HTMLElement;
-
-  // in future, check if it is day, week or month
 
   const tabType = activeCalendar.type;
 
@@ -126,8 +102,6 @@ export const updateCalendar = () => {
   } else {
     ("");
   }
-
-  console.log(activeCalendar.type);
 };
 
 // main.ts - loads once, displays the standard todo-list
